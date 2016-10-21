46c0022006f31a3d3ea10fab872e82b52e6561ad
Braulio Ríos Ferreira <braulioriosf@gmail.com>
2016-10-21 11:40:33 -0300

    Update butter_final.py

--- butter_final.py
+++ butter_final.py
@@ -2,9 +2,10 @@ from scipy.signal import butter, lfilter, square
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import freqz
+from scipy import interpolate
 
 
-def final_implementation(u):
+def final_implementation_100hz(u):
     y = [0.0]*len(u)
     for k in range(0, len(u)):
         u_2 = u[k-2] if k > 1 else 0
@@ -14,7 +15,17 @@ def final_implementation(u):
     return y
 
 
-def butter_bandpass(lowcut, highcut, fs, order=5):
+def final_implementation_400hz(u):
+    y = [0.0]*len(u)
+    for k in range(0, len(u)):
+        u_2 = u[k-2] if k > 1 else 0
+        y_2 = y[k-2] if k > 1 else 0
+        y_1 = y[k-1] if k > 0 else 0
+        y[k] = 0.16534236*(u[k] - u_2) + 1.66879379 * y_1 - 0.66931528 * y_2
+    return y
+
+
+def create_butter_bandpass(lowcut, highcut, fs, order=5):
     nyq = 0.5 * fs
     low = lowcut / nyq
     high = highcut / nyq
@@ -22,15 +33,18 @@ def butter_bandpass(lowcut, highcut, fs, order=5):
     return b, a
 
 
-def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
-    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+def apply_butter_bandpass(data, lowcut, highcut, fs, order=5):
+    b, a = create_butter_bandpass(lowcut, highcut, fs, order=order)
+    print("Numerator for order %d (fs = %dHz): " % (order, fs), b)
+    print("Denominator for order %d (fs = %dHz): " % (order, fs), a)
     y = lfilter(b, a, data)
     return y
 
 
 if __name__ == '__main__':
     # Sample rate and desired cutoff frequencies (in Hz).
-    fs = 100.0
+    fs1 = 100.0
+    fs2 = 4*fs1  # Sample rate for diagnostic mode
     lowcut = 0.1
     highcut = 25.0
 
@@ -40,13 +54,11 @@ if __name__ == '__main__':
     plt.close('all')
     # Plot the frequency response for a few different orders.
     f, (mag_plot, phase_plot) = plt.subplots(2, sharex=True)
-    for order in [1, 2]:
-        b, a = butter_bandpass(lowcut, highcut, fs, order=order)
-        print("Numerator for order %d: " % order, b)
-        print("Denominator for order %d: " % order, a)
+    for fs_i, order in [(fs1, 1), (fs1, 2), (fs2, 1)]:
+        b, a = create_butter_bandpass(lowcut, highcut, fs_i, order=order)
         w, h = freqz(b, a, worN=5000)
-        mag_plot.semilogx((fs * 0.5 / np.pi) * w, abs(h), label="order = %d" % (2*order))
-        phase_plot.semilogx((fs * 0.5 / np.pi) * w, np.angle(h)*180/np.pi, label="order = %d" % (2*order))
+        mag_plot.semilogx((fs_i / (2*np.pi)) * w, abs(h), label="order = %d, fs = %d" % (2 * order, fs_i))
+        phase_plot.semilogx((fs_i / (2*np.pi)) * w, np.angle(h) * 180 / np.pi, label="order = %d, fs = %d" % (2 * order, fs_i))
 
     mag_plot.set_title('Gain')
     phase_plot.set_title('Phase (deg)')
@@ -60,7 +72,7 @@ if __name__ == '__main__':
 
     # Filter noisy signal
     T = 10.0
-    nsamples = T * fs
+    nsamples = T * fs1
     t = np.linspace(0, T, nsamples, endpoint=False)
     a = 1
     x = 2  # DC offset
@@ -72,7 +84,7 @@ if __name__ == '__main__':
     # x += a * np.exp(-20 * (t - 4*T/8)**2) * np.cos(2 * np.pi * 50.0 * t)  # Attenuated freq
 
     # Add pulse of -1 amplitude and 0.5 seg
-    for k in range(20, 20 + int(fs/2)):
+    for k in range(20, 20 + int(fs1/2)):
         x[k] -= 1
 
     # x += a * square(2 * np.pi * f0 * t)
@@ -83,11 +95,26 @@ if __name__ == '__main__':
     plt.clf()
     plt.plot(t, x, label='Noisy signal')
 
-    for order in [1, 2]:
-        y = butter_bandpass_filter(x, lowcut, highcut, fs, order=order)
+    for order in [1]:  # , 2]:
+        y = apply_butter_bandpass(x, lowcut, highcut, fs1, order=order)
         plt.plot(t, y, label='Filtered signal order=%d' % (2*order))
-    #y = final_implementation(x)
-    #plt.plot(t, y, '+k', label='Final implementation')
+
+    # Filter signal in 400Hz
+    nsamples = T * fs2
+    # Redefine t and x for 400 samples/seg
+    x_interp = interpolate.interp1d(t, x)
+    t_400hz = np.linspace(0, t[len(t) - 1], nsamples, endpoint=False)
+    x_400hz = x_interp(t_400hz)
+
+    # y = butter_bandpass_filter(x, lowcut, highcut, fs2, order=1)
+    # plt.plot(t, y, 'y.', label='Filtered signal for 400Hz')
+
+    y = final_implementation_100hz(x)
+    plt.plot(t, y, '+k', label='Final implementation for 100Hz')
+
+    y_400hz = final_implementation_400hz(x_400hz)
+    plt.plot(t_400hz, y_400hz, '.y', label='Final implementation for 400Hz')
+
     plt.xlabel('time (seconds)')
     # plt.hlines([-a, a], 0, T, linestyles='--')
     plt.grid(True)
